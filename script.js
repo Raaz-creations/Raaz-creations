f9e643bd0cd75219b52ad9f133cb73867f688b5f
Raaz-creations <raazruny@gmail.com>
2024-11-06 21:47:14 +0100

    for her

---
+++ script.js
@@ -0,0 +1,5 @@
+// Function to reveal the secret message when the button is clicked
+function showSecret() {
+    const message = document.getElementById('secret-message');
+    message.classList.toggle('show');
+}
